ce760b7f79a7fab95922a00fc35d378cbe076537
Rumman Khan <khan0493@algonquinlive.com>
2023-02-08 05:03:26 -0500

    Finished Week 2

--- app/src/main/java/algonquin/cst2335/khan0493/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/khan0493/ui/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import algonquin.cst2335.khan0493.R;
 import algonquin.cst2335.khan0493.data.MainViewModel;
@@ -36,6 +37,42 @@ public class MainActivity extends AppCompatActivity {
 
         model.editString.observe(this, s -> {
             variableBinding.textview.setText("Your edit has " + s);
+        });
+
+        model.isSelected.observe(this, selected -> {
+            variableBinding.checkbox.setChecked(selected);
+            variableBinding.radioButton1.setChecked(selected);
+            variableBinding.switch1.setChecked(selected);
+
+            Toast.makeText(this, "The value is now: " + selected, Toast.LENGTH_SHORT).show();
+        });
+
+        variableBinding.checkbox.setOnCheckedChangeListener((checkbox, isChecked) -> {
+            if (isChecked) {
+                model.isSelected.postValue(isChecked);
+            }
+        });
+
+        variableBinding.radioButton1.setOnCheckedChangeListener((radioButton1, isChecked) -> {
+            if (isChecked) {
+                model.isSelected.postValue(isChecked);
+            }
+        });
+
+        variableBinding.switch1.setOnCheckedChangeListener((switch1, isChecked) -> {
+            if (isChecked) {
+                model.isSelected.postValue(isChecked);
+            }
+        });
+
+        variableBinding.myimagebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int width = variableBinding.myimagebutton.getWidth();
+                int height = variableBinding.myimagebutton.getHeight();
+                Toast.makeText(MainActivity.this,
+                        "The width = " + width + " and height = " + height, Toast.LENGTH_SHORT).show();
+            }
         });
 
     }
